aeefc24c62aa74b476515ffef9122ba7eda5aeea
Akshansh986 <akshansh@Akshanshs-Pro.lan>
2015-07-17 23:56:17 +0530

    Minor refactoring

--- Webkiosk/webkiosk/src/main/java/com/blackMonster/webkiosk/BroadcastReceivers/SponsorpayReceiver.java
+++ Webkiosk/webkiosk/src/main/java/com/blackMonster/webkiosk/BroadcastReceivers/SponsorpayReceiver.java
@@ -5,7 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
-public class YourReceiver extends BroadcastReceiver {
+public class SponsorpayReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
     	Log.d("YourReceiver", "Received");
