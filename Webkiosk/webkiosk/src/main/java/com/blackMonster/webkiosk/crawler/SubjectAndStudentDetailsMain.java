92d40ec299a4a8bd2d99fbc4d2f6f8d3abcdec18
Akshansh singh <akshanshsingh986@gmail.com>
2015-09-05 16:51:40 +0530

    light documentation in timetable and crawler

--- Webkiosk/webkiosk/src/main/java/com/blackMonster/webkiosk/crawler/SubjectAndStudentDetailsMain.java
+++ Webkiosk/webkiosk/src/main/java/com/blackMonster/webkiosk/crawler/SubjectAndStudentDetailsMain.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.regex.Matcher;
 
 /**
- * Created by akshansh on 07/07/15.
+ * Fetches data from "My attendance".
  */
 class SubjectAndStudentDetailsMain extends AbstractSubjectDetails {
 
