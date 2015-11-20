b58190352c7f171734cb1d3a45c9ab485788d036
Akshansh singh <akshanshsingh986@gmail.com>
2015-11-21 00:25:32 +0530

    MInor refactoring

--- Webkiosk/webkiosk/src/main/java/com/blackMonster/webkiosk/ui/MainActivity.java
+++ Webkiosk/webkiosk/src/main/java/com/blackMonster/webkiosk/ui/MainActivity.java
@@ -6,6 +6,9 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.blackMonster.webkiosk.SharedPrefs.RefreshDBPrefs;
+import com.blackMonster.webkiosk.WebkioskApp;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends Activity {
     public static final String TAG = "MainActivity";
@@ -14,9 +17,10 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         RefreshDBPrefs.resetIfrunningFromLongTime(this);
 
-        if (UIUtils.canViewAttendance(this)) {
+        if (WebkioskApp.isAppLogined(this)) {
             launchStartupActivity(this);
         } else {
             startActivity(new Intent(this, LoginActivity.class));
